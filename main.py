1a201ae415160551f72c0661294f541c48328a73
VladSkopenko <skopirka2k17@gmail.com>
2024-02-19 23:49:52 +0300

    Добавил таблицы, кислотные а так же сделал заготовку для crud с монго

--- main.py
+++ main.py
@@ -1,20 +1,67 @@
+import argparse
+
+from bson.objectid import ObjectId
 from password import password, user
 from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from tabulate import tabulate
+from colorama import Fore, Style, Back
+
 
 uri = f"mongodb+srv://{user}:{password}@goitlearn.x6ks5fo.mongodb.net/?retryWrites=true&w=majority"
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
+client = MongoClient(uri, server_api=ServerApi("1"))
 db = client.web_19
 
+parser = argparse.ArgumentParser(description="Test with cats and oleg")
+parser.add_argument("--action", help="create, update, read, delete") #CRUD
+parser.add_argument("--id")
+parser.add_argument("--name")
+parser.add_argument("--age")
+parser.add_argument("--features", nargs="+")
 
-db.cats.insert_one(
+arg = vars(parser.parse_args())
+action = arg.get("action")
+age = arg.get("age")
+pk = arg.get("id")
+name = arg.get("name")
+features = arg.get("features")
 
-        {"name": "Oleg",
-         "age": 22,
-         "Ya": "Love",
-         }
+def find():
+    return db.cats.find()
 
 
-)
+def create(name, age, features):
+    ...
 
+def update(pk, name, age, features):
+    ...
+
+
+def delete_of(pk):
+    ...
+
+
+def main():
+    match action:
+        case "create":
+            r = create(name, age, features)
+            print((r))
+        case "read":
+            r = find()
+            table = tabulate([e for e in r], headers="keys", tablefmt="grid")
+            header = f"{Fore.BLUE}{Back.WHITE}{Style.BRIGHT}"
+            table_colored = header + table.replace('\n', f"{Style.RESET_ALL}\n{header}") + Style.RESET_ALL
+            print(table_colored)
+
+        case "update":
+            r = update(pk, name, age, features)
+            print(r)
+        case "delete":
+            r = delete_of(pk)
+            print(r)
+        case _:
+            print("Unknown command")
+
+if __name__ == "__main__":
+    main()
