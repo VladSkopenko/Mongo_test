d4ac196934a2dbb54ee9c59db84ebb6d0a2ff8f0
VladSkopenko <skopirka2k17@gmail.com>
2024-02-20 00:15:57 +0300

    Added creare and upgrade for crud

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from pymongo.server_api import ServerApi
 from tabulate import tabulate
 from colorama import Fore, Style, Back
 
-
 uri = f"mongodb+srv://{user}:{password}@goitlearn.x6ks5fo.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(uri, server_api=ServerApi("1"))
 db = client.web_19
 
 parser = argparse.ArgumentParser(description="Test with cats and oleg")
-parser.add_argument("--action", help="create, update, read, delete") #CRUD
+parser.add_argument("--action", help="create, update, read, delete")  # CRUD
 parser.add_argument("--id")
 parser.add_argument("--name")
 parser.add_argument("--age")
@@ -27,15 +26,29 @@ pk = arg.get("id")
 name = arg.get("name")
 features = arg.get("features")
 
+
 def find():
     return db.cats.find()
 
 
 def create(name, age, features):
-    ...
+    result = db.cats.insert_one({
+        "name": name,
+        "age": int(age),
+        "features": features,
+    })
+    return result
+
 
 def update(pk, name, age, features):
-    ...
+    result = db.cats.update_one({"_id": ObjectId(pk)}, {
+        "$set": {                    #Это надо щоб если що была не замена полная а именно модификация
+            "name": name,
+            "age": int(age),
+            "features": features,
+        }
+    })
+    return result
 
 
 def delete_of(pk):
@@ -47,10 +60,11 @@ def main():
         case "create":
             r = create(name, age, features)
             print((r))
+
         case "read":
             r = find()
             table = tabulate([e for e in r], headers="keys", tablefmt="grid")
-            header = f"{Fore.BLUE}{Back.WHITE}{Style.BRIGHT}"
+            header = f"{Fore.GREEN}{Back.WHITE}{Style.BRIGHT}"
             table_colored = header + table.replace('\n', f"{Style.RESET_ALL}\n{header}") + Style.RESET_ALL
             print(table_colored)
 
@@ -62,6 +76,7 @@ def main():
             print(r)
         case _:
             print("Unknown command")
+
 
 if __name__ == "__main__":
     main()
